d037aea19a49b5e07f940c8244becbbe2ddbfb76
Reda Guemry <guemryreda@gmail.com>
2026-05-13 13:21:33 +0100

    add the walll for the pbstacle

--- Python/jeuxvideo.py
+++ Python/jeuxvideo.py
@@ -17,6 +17,13 @@ player_rect = player_surface.get_rect(center=(400, 300))
 box_surface = pygame.image.load('Python/img/box.png').convert_alpha()
 box_surface = pygame.transform.scale(box_surface, (60, 40))
 
+wall_surface = pygame.image.load('Python/img/wall.png').convert()
+wall_surface = pygame.transform.scale(wall_surface, (200, 90))
+wall_rect = wall_surface.get_rect(topleft=(0, 300))
+
+box_surface = pygame.image.load('Python/img/box.png').convert_alpha()
+box_surface = pygame.transform.scale(box_surface, (60, 40))
+
 box_rects = [
     box_surface.get_rect(center=(600, 400)),
     box_surface.get_rect(center=(200, 400)),
@@ -28,7 +35,9 @@ box_rects = [
 font = pygame.font.Font(None, 40)
 
 
-# ================= MAIN LOOP =================
+
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,19 +48,32 @@ while True:
 
     dx, dy = 0, 0
 
+    player_original = player_surface
+    current_player_surface = player_surface  
+
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         dx = 3
+        current_player_surface = pygame.transform.rotate(player_original, 0) 
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         dx = -3
+        current_player_surface = pygame.transform.rotate(player_original, 180) 
     if keys[pygame.K_UP] or keys[pygame.K_w]:
         dy = -3
+        current_player_surface = pygame.transform.rotate(player_original, 90) 
     if keys[pygame.K_DOWN] or keys[pygame.K_s]:
         dy = 3
-
+        current_player_surface = pygame.transform.rotate(player_original, 270) 
+    
+    screen.blit(current_player_surface, player_rect)
+    
     player_rect.x += dx
     
     if player_rect.left < 0: player_rect.left = 0
     if player_rect.right > 800: player_rect.right = 800
+    
+    if player_rect.colliderect(wall_rect):
+        if dx > 0: player_rect.right = wall_rect.left
+        if dx < 0: player_rect.left = wall_rect.right
 
     for box in box_rects:
         if player_rect.colliderect(box):
@@ -65,6 +87,14 @@ while True:
                 box.right = 800
                 player_rect.right = box.left
                 
+            if box.colliderect(wall_rect):
+                if dx > 0: 
+                    box.left = wall_rect.right
+                    player_rect.right = box.left
+                if dx < 0: 
+                    box.left = wall_rect.right
+                    player_rect.left = box.right
+                
             for other_box in box_rects:
                 if box != other_box and box.colliderect(other_box):
                     if dx > 0: 
@@ -76,6 +106,10 @@ while True:
 
     player_rect.y += dy
     
+    if player_rect.colliderect(wall_rect):
+        if dy > 0: player_rect.bottom = wall_rect.top
+        if dy < 0: player_rect.top = wall_rect.bottom
+
     if player_rect.top < 0: player_rect.top = 0
     if player_rect.bottom > 600: player_rect.bottom = 600
 
@@ -91,6 +125,15 @@ while True:
                 box.bottom = 600
                 player_rect.bottom = box.top
 
+
+            if box.colliderect(wall_rect):
+                if dy > 0: 
+                    box.bottom = wall_rect.top
+                    player_rect.bottom = box.top
+                if dy < 0: 
+                    box.top = wall_rect.bottom
+                    player_rect.top = box.bottom
+
             for other_box in box_rects:
                 if box != other_box and box.colliderect(other_box):
                     if dy > 0: 
@@ -99,15 +142,21 @@ while True:
                     if dy < 0: 
                         box.top = other_box.bottom
                         player_rect.top = box.bottom
+                        
+			
       
     screen.blit(background, (0, 0))
+    screen.blit(wall_surface, (0, 300))
 
     screen.blit(player_surface, player_rect)
-    pygame.draw.rect(screen, (0, 255, 0), player_rect, 2)
+
 
     for box in box_rects:
         screen.blit(box_surface, box)
         pygame.draw.rect(screen, (255, 0, 0), box, 2)
+
+    pygame.draw.rect(screen, (0, 255, 0), player_rect, 2)
+    pygame.draw.rect(screen, (0, 0, 255), wall_rect, 2)
 
     collision = any(player_rect.colliderect(box) for box in box_rects)
 
@@ -120,3 +169,11 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
+    
+
+
+
+
+
+
+
